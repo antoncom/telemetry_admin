c67510201ab8151a9c9f21debc561db847ec0e0d
antoncom <anton.com@mail.ru>
2017-10-03 07:35:37 +0300

    Добавлен $GetResourcesList(). Дорабтана стр. Тарифы

--- js/common.js
+++ js/common.js
@@ -21,3 +21,31 @@
     }
 
 })(jQuery);
+
+// GetResourcesList
+// How to use: $.GetResourcesList();
+// As result - $.Resources array is populated.
+(function($){
+    $.GetResourcesList = function() {
+        $.ajax({
+            type : 'GET',
+            url  : 'https://hometest.appix.ru/api/resources',
+            beforeSend: function(xhr){
+                xhr.setRequestHeader('X-Auth-Token', $.cookie('token'));
+                console.log('before ' + $.cookie('token'));
+            },
+            success :  function(response) {
+                var i;
+                $.Resources = new Array();
+                for(i = 0; i < response.length; i++){
+                    $.Resources[i] = { 'label': response[i].name, 'value' : response[i].id };
+                }
+            },
+            error :  function(response) {
+                console.log('ERROR GetResourcesList()');
+                console.log(JSON.stringify(response));
+            }
+        });
+    }
+
+})(jQuery);
