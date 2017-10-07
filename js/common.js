fbb88f391703d8c54313e5c867932d00a5ea2e7c
antoncom <anton.com@mail.ru>
2017-10-07 10:31:42 +0300

    стр. Дома и сектора, ogin referrer, dom_details

--- js/common.js
+++ js/common.js
@@ -49,3 +49,49 @@
     }
 
 })(jQuery);
+
+
+// Referrer - to redirect after login
+var pathname = window.location.pathname;
+var host = window.location.host;
+if(pathname.search(/login.html/i) == -1)    {
+    $.cookie('referrer', pathname);
+}
+
+// Get estate_id
+(function($){
+    $.GET = function(sParam) {
+        var sPageURL = window.location.search.substring(1);
+        var sURLVariables = sPageURL.split('&');
+        for (var i = 0; i < sURLVariables.length; i++) {
+            var sParameterName = sURLVariables[i].split('=');
+            if (sParameterName[0] == sParam) {
+                return sParameterName[1];
+            }
+        }
+    }
+})(jQuery);
+
+// Get Estate data
+(function($){
+    $.GetEstate = function(estId) {
+        $.ajax({
+            type : 'GET',
+            url  : 'https://hometest.appix.ru/api/estates/' + estId,
+            beforeSend: function(xhr){
+                xhr.setRequestHeader('X-Auth-Token', $.cookie('token'));
+                console.log('before ' + $.cookie('token'));
+            },
+            success :  function(response) {
+                //console.log(response);
+                return response;
+            },
+            error :  function(response) {
+                console.log('ERROR GetResourcesList()');
+                console.log(JSON.stringify(response));
+            }
+        });
+    }
+
+})(jQuery);
+
